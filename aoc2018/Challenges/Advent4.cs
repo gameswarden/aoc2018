27052076d19ad8b9d65ca94b5cc7f9f01ee31ed7
Warden <emailwarden@gmail.com>
2018-12-04 14:15:55 -0500

    Day 4 challenges

--- aoc2018/Challenges/Advent4.cs
+++ aoc2018/Challenges/Advent4.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using aoc2018.Util;
 
@@ -8,7 +11,7 @@ namespace aoc2018.Challenges
 {
     class Advent4 : Challenge
     {
-        public override string INPUT { get { return "C:/Dev/aoc2018/aoc2018/Input/3.txt"; } }
+        public override string INPUT { get { return "C:/Dev/aoc2018/aoc2018/Input/4.txt"; } }
 
 
         public override void Execute()
@@ -23,6 +26,13 @@ namespace aoc2018.Challenges
             Start();
             var input = Input.GetInputFromFile(INPUT);
 
+            var events = ParseEvents(input);
+
+            var guards = ParseEventsToGuards(events);
+
+            var sleepyGuard = guards.OrderByDescending(g => g.Value.Sum(v => v.Value)).First();
+
+            Console.WriteLine(sleepyGuard.Key * sleepyGuard.Value.OrderByDescending(v => v.Value).First().Key);
 
             End();
         }
@@ -33,8 +43,83 @@ namespace aoc2018.Challenges
             Start();
             var input = Input.GetInputFromFile(INPUT);
 
+            var events = ParseEvents(input);
+
+            var guards = ParseEventsToGuards(events);
+
+            var maxSleepingMinuteGuard = guards.OrderByDescending(g => g.Value.Any() ? g.Value.Max(v => v.Value) : 0).First();
+            var maxSleepingGuardMinute = maxSleepingMinuteGuard.Value.OrderByDescending(m => m.Value).First().Key;
+
+            Console.WriteLine(maxSleepingMinuteGuard.Key * maxSleepingGuardMinute);
+
             End();
 
         }
+
+        private SortedDictionary<int, SortedDictionary<int, int>> ParseEventsToGuards(SortedDictionary<DateTime, string> events)
+        {
+            var activeGuard = 0;
+            var sleepStart = 0;
+            var result = new SortedDictionary<int, SortedDictionary<int, int>>();
+
+            foreach (var e in events)
+            {
+                if (e.Value.Contains("begins shift"))
+                {
+                    var guardID = int.Parse(Regex.Match(e.Value, "[\\d]+").Value);
+                    activeGuard = guardID;
+                    if (!result.ContainsKey(activeGuard))
+                        result[activeGuard] = new SortedDictionary<int, int>();
+                    sleepStart = 0;
+                }
+                else if (e.Value == "falls asleep")
+                {
+                    sleepStart = e.Key.Minute;
+                }
+                else if (e.Value == "wakes up")
+                {
+                    for (int i = sleepStart; i < e.Key.Minute; i++)
+                    {
+                        if (!result[activeGuard].ContainsKey(i))
+                            result[activeGuard][i] = 0;
+                        result[activeGuard][i]++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private SortedDictionary<DateTime, string> ParseEvents(List<string> input)
+        {
+            var result = new SortedDictionary<DateTime, string>();
+
+            foreach (var i in input)
+            {
+                var match = Regex.Match(i, "(?:\\[)([\\d\\s-:]+)(?:[\\]\\s]+)([\\s\\w\\d#]+)");
+                var timeStamp = DateTime.Parse(match.Groups[1].Value);
+                var message = match.Groups[2].Value;
+
+                result[timeStamp] = message;
+            }
+
+            return result;
+        }
+    }
+
+    public class Event
+    {
+        public DateTime TimeStamp { get; set; }
+        public string Message { get; set; }
+
+        public static Event ParseEventString(string i)
+        {
+            var result = new Event();
+
+
+
+            return result;
+        }
     }
 }
+
--- aoc2018/Program.cs
+++ aoc2018/Program.cs
@@ -9,13 +9,16 @@ namespace aoc2018
         static void Main(string[] args)
         {
             Challenge challenge;
-            challenge = new Advent1();
+            /*challenge = new Advent1();
             challenge.Execute();
 
             challenge = new Advent2();
             challenge.Execute();
 
             challenge = new Advent3();
+            challenge.Execute();*/
+
+            challenge = new Advent4();
             challenge.Execute();
 
             Console.ReadLine();
